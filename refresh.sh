1182a7c327ac97a8136725446cbc588cda1afe89
emanuele-f <black.silver@hotmail.it>
2021-12-14 11:19:26 +0100

    Remove dashes from file names

--- refresh.sh
+++ refresh.sh
@@ -5,9 +5,9 @@ country_file="dbip-country-lite-${suffix}.mmdb.gz"
 asn_file="dbip-asn-lite-${suffix}.mmdb.gz"
 
 echo "$country_file ..."
-wget -qO dbip-country-lite.mmdb.gz https://download.db-ip.com/free/$country_file
-gzip -d dbip-country-lite.mmdb.gz
+wget -qO dbip_country_lite.mmdb.gz https://download.db-ip.com/free/$country_file
+gzip -d dbip_country_lite.mmdb.gz
 
 echo "$asn_file ..."
-wget -qO dbip-asn-lite.mmdb.gz https://download.db-ip.com/free/$asn_file
-gzip -d dbip-asn-lite.mmdb.gz
+wget -qO dbip_asn_lite.mmdb.gz https://download.db-ip.com/free/$asn_file
+gzip -d dbip_asn_lite.mmdb.gz
